3edad436aec4eccc13ec708b672e0069ed646b98
admin <admin@admins-MacBook-Air.local>
2016-08-06 09:35:17 +0530

    Fix typo

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'chef-email-notification'
 license          'All rights reserved'
-description      'Installs/Configures nginxldap'
+description      'Installs/Configures chef_handler to send notification emails when there is a failure using AWS SES'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'chef_handler'
